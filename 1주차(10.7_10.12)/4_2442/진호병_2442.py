2d5b2b0b949ddd24bab83d7cb33c34846650145a
jinhobyeong <jhb_0921@naver.com>
2022-10-09 02:54:57 +0900

    [진호병] 1주차 2442

---
+++ 1주차(10.7_10.12)/4_2442/진호병_2442.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+t = (n * 2 -1)
+
+for i in range(n):
+    for j in range(n-i-1): 
+        print(" ",end="")
+    for j in range(2*(i+1)-1):
+        print("*",end="")
+    print()
