96d6357fd3d47085c35dd8869a4307d7d711460d
baejunil <bjo6300@naver.com>
2022-10-14 18:47:01 +0900

    [배준일] 2주차 10988번 문제풀이
    다른 코드 테스트하다가 잘못된걸 올려서 수정한 코드입니다.

--- 2주차_10.11_10.17/3_10988/배준일_10988.py
+++ 2주차_10.11_10.17/3_10988/배준일_10988.py
@@ -1,10 +1,7 @@
-# # 팰린드롬인지 확인하기
+# 팰린드롬인지 확인하기
 
-# word = list(input())
-# if word == word[::-1]: # 현재 단어와 뒤집은 단어가 같으면 펠린드롬
-#     print('1')
-# else:
-#     print('0')
-
-a, b, c, d = 3, 4, 3, 5
-print(7 == b ^ c != a)
+word = list(input())
+if word == word[::-1]: # 현재 단어와 뒤집은 단어가 같으면 펠린드롬
+    print('1')
+else:
+    print('0')
