13caa3e6e58c635331ab41339e313a0f454c2487
jinhobyeong <jhb_0921@naver.com>
2022-10-22 22:31:10 +0900

    [진호병] 3주차 1026 문제풀이

--- 3주차_10.18_10.24/1_22864/진호병_22864.py
+++ 3주차_10.18_10.24/1_22864/진호병_22864.py
@@ -1,18 +1,18 @@
 A, B, C, M = map(int, input().split())
 
-currentA = 0
-totalC = 0
+currentA = 0 # 누적 피로도
+totalB = 0 # 총 일한 시간 
 
-if A > M:
+if A > M: # 1시간 일했을 때 피로도가 최대 피로도 보다 높을때 일할 수 없음
     print(0)
-else:
-    for i in range(24):
-        if currentA <= M - A:
-            currentA += A
-            totalC += B
-        else : 
-            currentA -= C
-            if currentA < 0:
+else: # 그렇지 않은 경우 
+    for i in range(24):  
+        if currentA <= M - A:  # 피로도가 추가될만큼의 여유가 있으면
+            currentA += A # 피로도 추가
+            totalB += B # 일한 시간 추가
+        else :  # 피로도 추가 여유가 없으면 쉬어야함
+            currentA -= C # 피로도 마이너스
+            if currentA < 0: # 피로도는 음수 x 
                 currentA = 0
 
-    print(totalC)
+    print(totalB)
