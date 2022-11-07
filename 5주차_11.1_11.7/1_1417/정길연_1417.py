46aa68857d9a79f1a9981fa284ee8c27327c2776
gilyeon00 <12venus08@naver.com>
2022-11-07 19:50:08 +0900

    [정길연] 1417번 문제풀이 (런타임오류 해결)

--- 5주차_11.1_11.7/1_1417/정길연_1417.py
+++ 5주차_11.1_11.7/1_1417/정길연_1417.py
@@ -9,20 +9,23 @@ for i in range(n):
     vote_list.append(int(input()))
 
 target = vote_list.pop(0)
+vote_list.sort(reverse=True)
 
-# 처음부터 가장 많은 득표 수을 얻었을 때 매수 불필요
-if target > max(vote_list) :
+if len(vote_list) == 0 :
     cnt = 0
-# 본인 제외 제일 많은 득표수가 본인 득표수와 같으면 1번만 매수
-elif target == max(vote_list) :
-    cnt = 1
 else : 
-    vote_list.sort(reverse=True)
-    while vote_list[0] >= target :
-        target += 1
-        vote_list[0] -= 1
-        cnt += 1
-        vote_list.sort(reverse=True)
+    # 처음부터 가장 많은 득표 수을 얻었을 때 매수 불필요
+    if target > vote_list[0] :
+        cnt = 0
+    # 본인 제외 제일 많은 득표수가 본인 득표수와 같으면 1번만 매수
+    elif target == vote_list[0] :
+        cnt = 1
+    else : 
+        while vote_list[0] >= target :
+            target += 1
+            vote_list[0] -= 1
+            cnt += 1
+            vote_list.sort(reverse=True)
 
 print(cnt)
 
