cbc22ebfe67f3f2fe6eb9ba7a686078c1a76ccdd
gilyeon00 <12venus08@naver.com>
2022-11-22 11:46:29 +0900

    [정길연] 7주차 4963번 문제풀이 - 테스트케이스는 다 맞았으나, 백준 채점이 틀리다고 나옴

---
+++ 07주차_11.16_11.22/3_4963/정길연_4963.py
@@ -0,0 +1,55 @@
+from collections import deque
+
+island = list()
+
+def bfs(a, b, graph):
+    dx = [1, -1, 0, 0, 1, -1, 1, -1]
+    dy = [0, 0, 1, -1, 1, -1, -1, 1]
+
+    q = deque()
+    q.append((b,a))
+
+    while q : 
+        y, x = q.popleft()
+
+        for i in range(8):      
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if (0<= nx < w and 0<= ny < h):
+                if graph[ny][nx] == 1:
+                    q.append((ny,nx))
+                    graph[ny][nx] = 0
+                   
+    return
+
+
+while True:
+    w, h = map(int, input().split())
+    island_cnt = 0 
+    if w == h == 0:
+        break
+    else:
+        graph = list()
+        for _ in range(h):
+            graph.append(list(map(int, input().split())))
+        
+        for i in range(h):
+            for j in range(w):
+                if graph[i][j] == 1:
+                    bfs(i, j, graph)
+                    island_cnt += 1
+                
+    island.append(island_cnt)
+
+for i in island:
+    print(i)
+
+
+
+
+
+
+    # for i in range(h):
+    #     for j in range(w):
+    #         print(graph[i][j], end= " ")
+    #     print()
