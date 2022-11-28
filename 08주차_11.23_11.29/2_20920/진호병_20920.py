fde60450024f99b41f7323ffba85b57aa6ccd01a
jinhobyeong <jhb_0921@naver.com>
2022-11-28 18:15:14 +0900

    [진호병] 8주차 20291 문제풀이

--- 08주차_11.23_11.29/2_20920/진호병_20920.py
+++ 08주차_11.23_11.29/2_20920/진호병_20920.py
@@ -10,11 +10,10 @@ for i in range(n):
     word = input().rstrip()
     if len(word) < m:
         continue
-    else :
-        if word in word_note : 
-            word_note[word] += 1
-        else : 
-            word_note[word] = 1
+    if word in word_note : 
+        word_note[word] += 1
+    else : 
+        word_note[word] = 1
 
 result = sorted(word_note.items(), key=lambda x:(-x[1], -len(x[0]) ,x[0]))
 
