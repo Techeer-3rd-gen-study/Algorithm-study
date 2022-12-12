bd93ff08d258828fd496e0e672d1f1f4ca478ff4
jinhobyeong <jhb_0921@naver.com>
2022-12-13 04:32:40 +0900

    [진호병] 10주차 2579 문제풀이

--- 10주차_12.07_12.13/1_14494/진호병_14494.py
+++ 10주차_12.07_12.13/1_14494/진호병_14494.py
@@ -1,17 +1,4 @@
-n = int(input())
 
-d = [0] * ((10 ** 6) + 1)
-
-for i in range(2, n+1):
-  d[i] = d[i-1] + 1
-  if i % 2 == 0 :
-    d[i] = min(d[i], d[i // 2] + 1)
-  if i % 3 == 0 :
-    d[i] = min(d[i], d[i // 3] + 1)
-  
-print(d[n])
-
-14494
 n, m = map(int, input().split())
 
 dp = [[0]*(m+1) for _ in range(n+1)]
