57bcd36427480b9c4c71e18e8386214a7272d170
jinhobyeong <jhb_0921@naver.com>
2022-12-12 23:25:27 +0900

    [진호병] 10주차 14494, 13301 문제풀이

--- 09주차_11.30_12.06/6_2110/진호병_2110.py
+++ 09주차_11.30_12.06/6_2110/진호병_2110.py
@@ -1,5 +1,3 @@
-
-
 import sys
 
 input = sys.stdin.readline
---
+++ 10주차_12.07_12.13/1_14494/진호병_14494.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+d = [0] * ((10 ** 6) + 1)
+
+for i in range(2, n+1):
+  d[i] = d[i-1] + 1
+  if i % 2 == 0 :
+    d[i] = min(d[i], d[i // 2] + 1)
+  if i % 3 == 0 :
+    d[i] = min(d[i], d[i // 3] + 1)
+  
+print(d[n])
+
+14494
+n, m = map(int, input().split())
+
+dp = [[0]*(m+1) for _ in range(n+1)]
+dp[1][1] = 1
+
+for i in range(1, n+1) :
+  for j in range(1, m+1):
+    if i == 1 and j == 1: 
+      continue
+
+    dp[i][j] = dp[i-1][j] + dp[i][j-1] + dp[i-1][j-1]
+
+print(dp[n][m] % 1000000007)
