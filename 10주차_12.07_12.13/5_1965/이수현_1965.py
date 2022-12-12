dd97816f6ac76a03f6182842b18a97952aef83ef
suhyeon <suhyeon3484@naver.com>
2022-12-12 19:37:17 +0900

    [이수현] 10주차 1965번 문제풀이

---
+++ 10주차_12.07_12.13/5_1965/이수현_1965.py
@@ -0,0 +1,15 @@
+# 1965 : 상자 넣기
+
+n = int(input())
+box = list(map(int, input().split()))
+
+# dp 테이블 1로 초기화
+d = [1] * 1001
+
+for i in range(1, n):
+    for j in range(i):
+        # 나보다 작은 상자가 있을 시
+        if box[j] < box[i]:
+            d[i] = max(d[i], d[j] + 1)
+            
+print(max(d))
