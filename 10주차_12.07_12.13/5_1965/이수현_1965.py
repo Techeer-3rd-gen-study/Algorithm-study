74100eddd35494725e119e6fc28c062d3f3ef178
suhyeon <suhyeon3484@naver.com>
2022-12-12 19:42:50 +0900

    [이수현] 1965번 주석 수정

--- 10주차_12.07_12.13/5_1965/이수현_1965.py
+++ 10주차_12.07_12.13/5_1965/이수현_1965.py
@@ -8,7 +8,7 @@ d = [1] * 1001
 
 for i in range(1, n):
     for j in range(i):
-        # 나보다 작은 상자가 있을 시
+        # 현재 위치에서 앞에 더 작은 상자가 있을 시
         if box[j] < box[i]:
             d[i] = max(d[i], d[j] + 1)
             
