d83406ac75a06ef425dda4e4632752c751aa7e0d
baejunil <bjo6300@naver.com>
2022-12-15 15:37:47 +0900

    [배준일] 11주차 1890번 문제풀이

--- 11주차_12.14_12.20/1_11048/배준일_11048.py
+++ 11주차_12.14_12.20/1_11048/배준일_11048.py
@@ -4,12 +4,9 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().rstrip().split()) 
-candy = []
+candy = [list(map(int, input().rstrip().split())) for _ in range(n)]
 dp = [[0] * (m + 1) for _ in range(n + 1)]
 # dp = [[0] * (m + 1)] * (n + 1) 으로 값을 선언하면 dp[i][1]이 변경될 때 dp[0][1], dp[1][1], dp[2][1], dp[3][1]이 다 변경된다. 주의하자.
-
-for _ in range(n):
-    candy.append(list(map(int, input().rstrip().split())))
 
 for i in range(1, n + 1):
     for j in range(1, m + 1):
