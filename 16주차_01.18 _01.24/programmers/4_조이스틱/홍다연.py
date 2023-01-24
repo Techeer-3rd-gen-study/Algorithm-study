6fc48e8b435a9e1e5dc39240f141d3e97c04ccfe
Dayeon-Hong <dayon0325@gmail.com>
2023-01-24 11:41:12 +0900

    [홍다연] 16주차 프로그래머스 4번 문제풀이
    조이스틱

---
+++ 16주차_01.18 _01.24/programmers/4_조이스틱/홍다연.py
@@ -0,0 +1,22 @@
+# 조이스틱
+
+def solution(name):
+  
+    answer = 0
+    min_move = len(name) - 1
+    
+    for i, char in enumerate(name):
+    	# 해당 알파벳 변경 최솟값 추가
+        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+        
+        next = i + 1
+
+        while next < len(name) and name[next] == 'A':
+            next += 1
+            
+        # 기존, 연속된 A의 왼쪽시작 방식, 연속된 A의 오른쪽시작 방식 비교 및 갱신
+        min_move = min([min_move, 2 *i + len(name) - next, i + 2 * (len(name) -next)])
+        
+    answer += min_move
+
+    return answer
