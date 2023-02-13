aa27a0dd71ffe309d53fc3ffc9bc9f90d09941e2
suhyeon <suhyeon3484@naver.com>
2023-02-14 03:49:21 +0900

    [이수현] 19주차 피로도 문제풀이

---
+++ 19주차_02.08_02.14/programmers/4_네트워크/이수현.py
@@ -0,0 +1,17 @@
+# 네트워크
+
+def solution(n, computers):
+    answer = 0
+    visited = [False for i in range(n)]
+    for com in range(n):
+        if visited[com] == False:
+            DFS(n, computers, com, visited)
+            answer += 1 #DFS로 최대한 컴퓨터들을 방문하고 빠져나오게 되면 그것이 하나의 네트워크.
+    return answer
+
+def DFS(n, computers, com, visited):
+    visited[com] = True
+    for connect in range(n):
+        if connect != com and computers[com][connect] == 1: #연결된 컴퓨터
+            if visited[connect] == False:
+                DFS(n, computers, connect, visited)
