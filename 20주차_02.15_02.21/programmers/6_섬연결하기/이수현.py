d5ccce858cca2aaddd275e54768697f2954fb2d2
suhyeon <suhyeon3484@naver.com>
2023-02-21 02:50:34 +0900

    [이수현] 20주차 섬연결하기 문제풀이

---
+++ 20주차_02.15_02.21/programmers/6_섬연결하기/이수현.py
@@ -0,0 +1,19 @@
+# 섬 연결하기
+# https://soohyun6879.tistory.com/145
+
+def solution(n, costs):
+    answer = 0
+    costs.sort(key = lambda x: x[2]) # 비용기준으로 오름차순 정렬
+    connect = set([costs[0][0]]) # 연결을 확인하는 집합
+    
+    # Kruskal 알고리즘으로 최소 비용 구하기
+    while len(connect) != n:
+        for cost in costs:
+            if cost[0] in connect and cost[1] in connect:
+                continue
+            if cost[0] in connect or cost[1] in connect:
+                connect.update([cost[0], cost[1]])
+                answer += cost[2]
+                break
+                
+    return answer
